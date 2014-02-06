39c48a3640a3962d8e26ed13c91972958ade3d7c
brn.stabile@gmail.com <brn.stabile@gmail.com@92f57c79-4fe0-0ef1-effe-11e161ba34e9>
2014-02-06 19:54:31 +0000

    Adicionando conteudo ao diretório de teste cliente GWT.

---
+++ trunk/ClienteTestes/client/src/es/deusto/weblab/client/experiments/corponegro/ui/CorpoNegroExperiment.java
@@ -0,0 +1,87 @@
+/*
+* Copyright (C) 2012 onwards University of Deusto
+* All rights reserved.
+*
+* This software is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.
+*
+* This software consists of contributions made by many individuals, 
+* listed below:
+*
+* Author: FILLME
+*
+*/
+
+package es.deusto.weblab.client.experiments.corponegro.ui;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import es.deusto.weblab.client.configuration.IConfigurationRetriever;
+import es.deusto.weblab.client.lab.experiments.IBoardBaseController;
+import es.deusto.weblab.client.lab.experiments.UIExperimentBase;
+import es.deusto.weblab.client.ui.widgets.WlTimer;
+import es.deusto.weblab.client.ui.widgets.WlWebcam;
+
+public class CorpoNegroExperiment extends UIExperimentBase{
+
+	public CorpoNegroExperiment(IConfigurationRetriever configurationRetriever,
+			IBoardBaseController boardController) {
+		super(configurationRetriever, boardController);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	@Override
+	public void initialize(){
+		putWidget(new VerticalPanel());
+	}
+	
+	@Override
+	public JSONValue getInitialData() {
+		return null;
+
+	}
+	
+	@Override
+	public void queued(){
+		putWidget(new VerticalPanel());
+	}
+	
+    @Override
+    public void start(int time, String initialConfiguration) {
+
+    	final JSONValue value = JSONParser.parseStrict(initialConfiguration);
+    	final JSONObject obj = (JSONObject)value;
+    	
+		final MainPanel mainPanel = new MainPanel();
+    	
+		// 
+		// Configure the camera
+		final WlWebcam camera = mainPanel.getWebcam();
+		camera.configureWebcam(obj);
+		camera.start();
+		addDisposableWidgets(camera);
+		
+		//
+		// Configure the timer
+		final WlTimer timer = mainPanel.getTimer();
+		timer.updateTime(time);
+		addDisposableWidgets(timer);
+    	
+		putWidget(mainPanel);
+    }
+
+	
+}
+
+/* Useful commands
+ * 
+ *  boardController.sendCommand
+ *  
+ *  putWidget
+ *  addDisposableWidgets
+ * */
+ 
---
+++ trunk/ClienteTestes/client/src/es/deusto/weblab/client/experiments/corponegro/ui/MainPanel.java
@@ -0,0 +1,47 @@
+/*
+* Copyright (C) 2012 onwards University of Deusto
+* All rights reserved.
+*
+* This software is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.
+*
+* This software consists of contributions made by many individuals, 
+* listed below:
+*
+* Author: FILLME
+*
+*/
+
+package es.deusto.weblab.client.experiments.corponegro.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import es.deusto.weblab.client.ui.widgets.WlWebcam;
+import es.deusto.weblab.client.ui.widgets.WlTimer;
+
+public class MainPanel extends Composite {
+
+	private static MainPanelUiBinder uiBinder = GWT
+			.create(MainPanelUiBinder.class);
+	
+	@UiField WlWebcam camera;
+	@UiField WlTimer timer;
+
+	interface MainPanelUiBinder extends UiBinder<Widget, MainPanel> {
+	}
+
+	public MainPanel() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	public WlWebcam getWebcam() {
+		return this.camera;
+	}
+	
+	public WlTimer getTimer(){
+		return this.timer;
+	}
+}
