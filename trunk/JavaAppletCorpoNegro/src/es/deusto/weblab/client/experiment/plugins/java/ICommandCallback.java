d9f5fc9d74606719599b75cf3d9257ed8e76a267
brn.stabile@gmail.com <brn.stabile@gmail.com@92f57c79-4fe0-0ef1-effe-11e161ba34e9>
2014-03-13 21:01:23 +0000

    Organizando experimento Corpo Negro

--- trunk/JavaAppletCorpoNegro/src/es/deusto/weblab/client/experiment/plugins/java/ICommandCallback.java
+++
@@ -1,6 +0,0 @@
-package es.deusto.weblab.client.experiment.plugins.java;
-
-public interface ICommandCallback {
-	public void onSuccess(ResponseCommand response);
-	public void onFailure(String message);
-}
