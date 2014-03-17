39bac6f8d8fcb6d785206c93bc39751646f3f9cd
brn.stabile@gmail.com <brn.stabile@gmail.com@92f57c79-4fe0-0ef1-effe-11e161ba34e9>
2014-03-17 20:01:12 +0000

    Organizando projetos

---
+++ trunk/0_Beta/voz/servidor-voz/src/net/sourceforge/peers/gui/main.java
@@ -0,0 +1,19 @@
+package net.sourceforge.peers.gui;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+
+		
+		MainFrame.main(new String[]{});
+
+		
+		
+	}
+
+}
