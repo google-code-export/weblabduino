b40d8fe21dd35d710dab917f3da9ae466df040a4
brn.stabile@gmail.com <brn.stabile@gmail.com@92f57c79-4fe0-0ef1-effe-11e161ba34e9>
2015-01-15 00:29:38 +0000

    organizacao da interface do experimento CNA

---
+++ CorpoNegro/client/gwt/client/src/es/deusto/weblab/client/Weblabteste.java
@@ -0,0 +1,46 @@
+/*
+* Copyright (C) 2012 onwards University of Deusto
+* All rights reserved.
+*
+* This software is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.
+*
+* This software consists of contributions made by many individuals, 
+* listed below:
+*
+* Author: FILLME
+*
+*/
+
+package es.deusto.weblab.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import es.deusto.weblab.client.configuration.IConfigurationRetriever;
+import es.deusto.weblab.client.experiments.corponegro.CorpoNegroCreatorFactory;
+import es.deusto.weblab.client.experiments.corponegro.ui.MainPanel;
+
+public class Weblabteste extends WebLabClient implements EntryPoint{
+
+	MainPanel mp = new MainPanel();
+	CorpoNegroCreatorFactory ccc = new CorpoNegroCreatorFactory();
+	
+	@Override
+	public void onModuleLoad() {
+		this.loadApplication();
+		// TODO Auto-generated method stub
+
+
+	}
+
+	@Override
+	public void loadApplication() {
+		// TODO Auto-generated method stub
+		ccc.createExperimentCreator(Weblabteste.this.configurationManager);
+		RootPanel.get().add(mp);
+		
+	}
+
+
+}
