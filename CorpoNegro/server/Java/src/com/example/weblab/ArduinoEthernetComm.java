f368c5c475acb1a7e9ebf3bd776c1ffe7eba5c4f
brn.stabile@gmail.com <brn.stabile@gmail.com@92f57c79-4fe0-0ef1-effe-11e161ba34e9>
2015-01-15 20:06:44 +0000

    Adicionando bibliotecas no servidor CNA

--- CorpoNegro/server/Java/src/com/example/weblab/ArduinoEthernetComm.java
+++ CorpoNegro/server/Java/src/com/example/weblab/ArduinoEthernetComm.java
@@ -1,7 +1,9 @@
 package com.example.weblab;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 
 public class ArduinoEthernetComm
