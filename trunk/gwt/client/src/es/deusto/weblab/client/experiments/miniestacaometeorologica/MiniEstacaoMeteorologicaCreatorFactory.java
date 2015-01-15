fcc45d4d1bceadec7e6aa275fd631bb7918b1967
brn.stabile@gmail.com <brn.stabile@gmail.com@92f57c79-4fe0-0ef1-effe-11e161ba34e9>
2015-01-15 00:53:19 +0000

    organizando experimentos num unico gwt

---
+++ trunk/gwt/client/src/es/deusto/weblab/client/experiments/miniestacaometeorologica/MiniEstacaoMeteorologicaCreatorFactory.java
@@ -0,0 +1,64 @@
+/*
+* Copyright (C) 2012 onwards University of Deusto
+* All rights reserved.
+*
+* This software is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.
+*
+* This software consists of contributions made by many individuals, 
+* listed below:
+*
+* Author: Bruno Stabile - brn.stabile@gmail.com
+*
+*/
+
+package es.deusto.weblab.client.experiments.miniestacaometeorologica;
+
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
+
+import es.deusto.weblab.client.configuration.IConfigurationRetriever;
+import es.deusto.weblab.client.experiments.miniestacaometeorologica.ui.MiniEstacaoMeteorologicaExperiment;
+import es.deusto.weblab.client.lab.experiments.ExperimentCreator;
+import es.deusto.weblab.client.lab.experiments.ExperimentFactory.IExperimentLoadedCallback;
+import es.deusto.weblab.client.lab.experiments.ExperimentFactory.MobileSupport;
+import es.deusto.weblab.client.lab.experiments.IBoardBaseController;
+import es.deusto.weblab.client.lab.experiments.IExperimentCreatorFactory;
+
+public class MiniEstacaoMeteorologicaCreatorFactory implements IExperimentCreatorFactory{
+
+	@Override
+	public String getCodeName() {
+
+		return "mini-estacao-meteorologica";
+	}
+
+	@Override
+	public ExperimentCreator createExperimentCreator(
+			final IConfigurationRetriever configurationRetriever) {
+		
+		return new ExperimentCreator(MobileSupport.limited, getCodeName()){
+			
+			@Override
+			public void createWeb(final IBoardBaseController boardController, final IExperimentLoadedCallback callback) {
+				GWT.runAsync(new RunAsyncCallback() {
+					
+					@Override
+					public void onSuccess() {
+						callback.onExperimentLoaded(new MiniEstacaoMeteorologicaExperiment(
+								configurationRetriever,
+								boardController
+							));
+					}
+					
+					@Override
+					public void onFailure(Throwable e){
+						callback.onFailure(e);
+					}
+				});
+			}
+		};
+	}
+	
+}
