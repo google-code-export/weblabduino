ee4e13446d10098801c89800d744787f94a34f92
brn.stabile@gmail.com <brn.stabile@gmail.com@92f57c79-4fe0-0ef1-effe-11e161ba34e9>
2015-01-16 00:04:06 +0000

    Inclusao de envio de dados no .ino do experimento CNA

--- CorpoNegro/ino/corponegro/corponegro.ino
+++ CorpoNegro/ino/corponegro/corponegro.ino
@@ -3,7 +3,6 @@
 
 //Endereco MAC para o shield Ethernet
 byte mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x3a };
-unsigned long tempo = millis();
 
 //Pino de controle da lampada
 int lampada = 6;
@@ -16,126 +15,120 @@ char command = 0;
 //Comando recebido com sucesso
 boolean runCommand = false;
 
+//Gets a client that is connected to the server and has data available for reading
+EthernetClient client;
 
 // Acesso ao shield ethernet pela porta 80
 EthernetServer server(80);
 
 void setup() {
 
-// Inicia-se a comunicaço serial e aguarda uma resposta
-Serial.begin(9600);
-while (!Serial) {
-; // wait for serial port to connect. Needed for Leonrdo only
-}
+  // Inicia-se a comunicaço serial e aguarda uma resposta
+  Serial.begin(9600);
+  
+  conectarRede();
 
-// Inicia-se o shield ethernet com o MAC configurado anteriormente
-Ethernet.begin(mac);
-server.begin();
-
-Serial.print("IP: ");
-Serial.println(Ethernet.localIP());
-
-pinMode(lampada,OUTPUT);
-digitalWrite(lampada,LOW);
+  pinMode(lampada,OUTPUT);
+  digitalWrite(lampada,LOW);
 
 }
 
 
 void loop() {
-
-// Verificar clients e receber comandos via ethernet.
-checkForCommands();
-
-if(runCommand){
-
-runCommand = false;
   
-Serial.print("Executando comando [");
-Serial.print(command);
-Serial.println("] enviado...");
-
-switch(command){
-
-case 'l':
-controlarLampada();
-break;
-
-case 'd':
-temperaturaCalotas();
-break;
-
-case '9':
-resetExperimento();
-break;
-
-default:
-Serial.println("Nenhum comando executado");
-break;
-
-}
-
-}
-
-}
-
-void checkForCommands(){
-
-//Gets a client that is connected to the server and has data available for reading
-EthernetClient client = server.available();
-
-//Verifica se existe um cliente conectado
-if (client) {
-
-Serial.println("Cliente Conectado");
-
-//Verifica se existem dados remanescentes do cliente, conectado ou nao
-if (client.connected()) {
-
-c = client.read();//Faz a leitura de um caracter
-Serial.print("Caracter recebido [");
-Serial.print(c);
-Serial.println("]");
-
-if ( c >= 0 & c <= 127 ){// Testa se o caracter é valido. (client.read irá enviar -1 para sinalizar que não existem mais dados)
-
-Serial.println("Caracter valido");
-
-}else{
-
-Serial.println("Caracter nao reconhecido na tabela ASCII, finalizando conexao");
-client.flush(); //Exclusao de qualquer dado remanescente dos clients
-client.stop(); // Fechar qualquer conexao
-
-}
-
-//checar sinalizacao do final do comando
-if(c == 13 || c == 10 || c== -1){
-Serial.println("Caracter de finalização detectado, encerrando conexao");
-client.flush(); //Exclusao de qualquer dado remanescente dos clients
-client.stop(); // Fechar qualquer conexao
-}
-
-command = c ;
-Serial.print("Caracter [");
-Serial.print(command);
-Serial.println("] armazenado");
-runCommand = true;
-
-client.flush(); //Exclusao de qualquer dado remanescente dos clients
-client.stop(); // Fechar qualquer conexao
-
-
-}
-
-}
-
-}
-
-void temperaturaCalotas(){
-
-//Gets a client that is connected to the server and has data available for reading
-EthernetClient client = server.available();
+  client = server.available();
+   
+  // Verificar clients e receber comandos via ethernet.
+  checkForCommands(client);
   
+  if(runCommand){
+  
+  runCommand = false;
+    
+  Serial.print("Executando comando [");
+  Serial.print(command);
+  Serial.println("] enviado...");
+  
+    switch(command){
+    
+    case 'l':
+    controlarLampada();
+    client.flush(); //Exclusao de qualquer dado remanescente dos clients
+    client.stop(); // Fechar qualquer conexao
+    break;
+    
+    case 'd':
+    temperaturaCalotas(client);
+    client.flush(); //Exclusao de qualquer dado remanescente dos clients
+    client.stop(); // Fechar qualquer conexao
+    
+    break;
+    
+    case '9':
+    resetExperimento();
+    client.flush(); //Exclusao de qualquer dado remanescente dos clients
+    client.stop(); // Fechar qualquer conexao
+    break;
+    
+    default:
+    Serial.println("Comando nao cadastrado");
+    client.flush(); //Exclusao de qualquer dado remanescente dos clients
+    client.stop(); // Fechar qualquer conexao
+    break;
+    
+    }
+  
+  }
+
+}
+
+void checkForCommands(EthernetClient client){
+  
+
+  //Verifica se existe um cliente conectado
+  if (client) {
+  
+  Serial.println("Cliente Conectado");
+  
+  //Verifica se existem dados remanescentes do cliente, conectado ou nao
+  if(client.connected()) {
+  
+    c = client.read();//Faz a leitura de um caracter
+    command = c ;
+    Serial.print("Caracter recebido [");
+    Serial.print(c);
+    Serial.println("]");
+
+    runCommand = true;
+    
+    if ( c >= 0 & c <= 127 ){// Testa se o caracter é valido. (client.read irá enviar -1 para sinalizar que não existem mais dados)
+    
+    Serial.println("Caracter valido");
+    
+    }else{
+    
+    Serial.println("Caracter nao reconhecido na tabela ASCII, finalizando conexao");
+    client.flush(); //Exclusao de qualquer dado remanescente dos clients
+    client.stop(); // Fechar qualquer conexao
+    runCommand = false; 
+    }
+    
+    //checar sinalizacao do final do comando
+    if(c == 13 || c == 10 || c== -1){
+    Serial.println("Caracter de finalização detectado, encerrando conexao");
+    client.flush(); //Exclusao de qualquer dado remanescente dos clients
+    client.stop(); // Fechar qualquer conexao
+    runCommand = false; 
+    }
+        
+    }
+  
+  }
+
+}
+
+void temperaturaCalotas(EthernetClient client){
+
 int i;
 int calotaBrancaAnalog = A0;
 int calotaPretaAnalog = A1;
@@ -146,7 +139,6 @@ calotaBrancaAnalog = calotaBrancaAnalog + analogRead(A0);
 calotaPretaAnalog = calotaPretaAnalog + analogRead(A1);
 }
 
-
 //Media das 5 amostras
 calotaBrancaAnalog = calotaBrancaAnalog/5;
 calotaPretaAnalog = calotaPretaAnalog/5;
@@ -156,11 +148,12 @@ float temperaturaCalotaPreta=(5*calotaPretaAnalog*100)/1023;
 
 Serial.print(temperaturaCalotaPreta);
 Serial.print(" ");
-Serial.print(temperaturaCalotaBranca);
-Serial.print(" ");
-Serial.println(tempo);
+Serial.println(temperaturaCalotaBranca);
 
-server.write(temperaturaCalotaPreta);
+client.print('#');
+client.print(temperaturaCalotaBranca);
+client.print(',');
+client.println(temperaturaCalotaPreta);
 
 delay(100);
 }
@@ -178,9 +171,27 @@ if(statusLampada == 1){
 }
 }
 
+void conectarRede(){
+  
+  if(!Ethernet.begin(mac)){
+    Serial.println("conectando com a rede...");
+    Ethernet.begin(mac);
+    delay(1000);
+  }
+  delay(200);
+  system("/etc/init.d/networking restart");
+  delay(200);
+  server.begin();
+  delay(200);
+  Serial.print("IP: ");
+  Serial.println(Ethernet.localIP());
+  
+}
+
 void resetExperimento(){
 
   Serial.println("Reiniciando experimento...");
+  conectarRede();
   digitalWrite(lampada,LOW);
   
 }
