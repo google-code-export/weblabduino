43a668b03bfcab25f54086fa569cfb5c07364a6f
brn.stabile@gmail.com <brn.stabile@gmail.com@92f57c79-4fe0-0ef1-effe-11e161ba34e9>
2015-01-28 21:52:19 +0000

    atulização ino cna

--- trunk/CorpoNegro/server/Java/src/com/example/weblab/ConexaoMysql.java
+++ trunk/CorpoNegro/server/Java/src/com/example/weblab/ConexaoMysql.java
@@ -1,16 +1,20 @@
 package com.example.weblab;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class ConexaoMysql {
    // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/experimentos";
+   static final String DB_URL = "jdbc:mysql://192.168.42.1/experimentos";
 
    //  Database credentials
-   static final String USER = "cna";
-   static final String PASS = "cna";
-      
+   static final String USER = "experimentos";
+   static final String PASS = "p=r*i2";
+     
    public void EnviarDadosDB(String dados) {
    Connection conn = null;
    Statement stmt = null;
@@ -28,13 +32,13 @@ public class ConexaoMysql {
       System.out.println("Creating statement...");
       stmt = conn.createStatement();
       String sql;
-    		        
+                       
       String[] valores = dados.split("-");
 
-      sql = "INSERT INTO `cna`(`ch0`, `ch1`, `data`) VALUES ("+valores[0]+","+valores[1]+",CURRENT_TIMESTAMP);";
-      
+      sql = "INSERT INTO `cna`(`data`, `ch0`, `ch1`) VALUES (CURRENT_TIMESTAMP,"+valores[0]+","+valores[1]+");";
+     
       stmt.executeUpdate(sql);
-      
+     
       //STEP 6: Clean-up environment
       stmt.close();
       conn.close();
